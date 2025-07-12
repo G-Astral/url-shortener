fa1c3841fc7955d8d293b979ee6c5c037ef061d3
G-Astral <super.anakonda2001@yandex.ru>
2025-07-12 17:18:48 +0300

    DB init

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,12 +5,14 @@ import (
 	"os"
 
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 )
 
 const (
 	envLocal = "local"
-	envDev = "dev"
-	envProd = "prod"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -21,10 +23,17 @@ func main() {
 	log.Debug("debug messages are enabled")
 
 	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	// TODO: init router: chi, "chi render"
 
-	// TODO: run server: 
+	// TODO: run server:
 }
 
 func setupLogger(env string) *slog.Logger {
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,15 +9,15 @@ import (
 )
 
 type Config struct {
-	Env 			string 			`yaml:"env" env-default:"local"`
-	StoragePath 	string 			`yaml:"storage_path" env-required:"true"`
-	HTTPServer 						`yaml:"http_server"`
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-	Address 		string 			`yaml:"address" env-default:"localhost:8080"`
-	Timeout 		time.Duration 	`yaml:"timeout" env-default:"4s"`
-	IdleTimeout 	time.Duration 	`yaml:"idle_timeout" env-default:"60s"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
@@ -26,7 +26,6 @@ func MustLoad() *Config {
 		configPath = "./config/local.yaml"
 		log.Printf("CONFIG_PATH is not set. Used deafult value")
 	}
-
 
 	// check if file exists
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
@@ -37,7 +36,7 @@ func MustLoad() *Config {
 
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
 		log.Fatalf("cannot read config: %s", err)
-	}	
+	}
 
 	return &cfg
 }
