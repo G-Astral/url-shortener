bca39bdb3d914140b179ecbfca30d0d33d091473
G-Astral <super.anakonda2001@yandex.ru>
2025-07-13 23:26:49 +0300

    Redirect by alias add + tests successfull

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
 	"url-shortener/internal/lib/logger/handlers/slogpretty"
@@ -46,6 +47,7 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(log, storage))
+	router.Get("/{alias}", redirect.New(log, storage))
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
